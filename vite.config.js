ecd89abc3810ced6b8c5229141dd104d7039a75c
Pokefish <fabriciowarlock@gmail.com>
2026-07-26 00:11:02 -0300

    Enhance icon settings for maskable support
    Updated icon configuration to include purpose for maskable icons.

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,18 @@ export default defineConfig({
         scope: '/',
         start_url: '/',
         icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+          { 
+            src: '/icon-192.png', 
+            sizes: '192x192', 
+            type: 'image/png',
+            purpose: 'any maskable' // Isso ajuda o ícone a se adaptar a diferentes formatos de tela
+          },
+          { 
+            src: '/icon-512.png', 
+            sizes: '512x512', 
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
         ]
       }
     })
